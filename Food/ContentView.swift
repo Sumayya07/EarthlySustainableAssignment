a2fdcbde24ea6ce8d2f055966d4efad00164add4
Sumayya07 <sumayyasiddiqui7173@gmail.com>
2023-07-27 00:39:42 +0530

    Initial Commit

---
+++ Food/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food
+//
+//  Created by Sumayya on 26/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            OnBoarding()
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by Sumayya on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
